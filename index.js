8409843ee15cc32c13b2cccd241a43d6b7cd2df2
Buddhika Shahan <100762985+buddhikashahan@users.noreply.github.com>
2024-04-24 02:56:05 +0000

    Added youtube downloader

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const instagram = require('./lib/instagram');
 const facebook = require('./lib/facebook');
 const tiktok = require('./lib/tiktok');
+const youtube = require('./lib/youtube');
 
 module.exports = {
     instagramDown: instagram.instagramDown,
     facebookDown: facebook.facebookDown,
     tiktokDown: tiktok.tiktokDown,
+    youtubeDown: youtube.youtubeDown,
 };
